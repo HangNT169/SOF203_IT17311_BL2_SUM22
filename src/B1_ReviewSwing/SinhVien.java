9d96cfb6e5755767463a9f1e4ed7f676996575a2
HangNT164 <50843516+HangNT164@users.noreply.github.com>
2022-06-28 09:04:27 +0700

    B1_ReviewSwing

---
+++ src/B1_ReviewSwing/SinhVien.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package B1_ReviewSwing;
+
+/**
+ *
+ * @author hangnt
+ */
+public class SinhVien {
+
+    /**
+     * // * Bien + Ham protected : // * - Trong cung 1 class // * - Khac class
+     * chi goi duoc trong cung 1 package // * - Khac package thi co the goi
+     * thong qua phuong thuc ke thua extends // *
+     * https://quachquynh.com/private-public-protected-trong-java/ // * // *
+     * Default: Trong cung 1 class hoac package thoi //
+     */
+    private String ten;
+    private String loai;
+    private boolean gioiTinh;
+    private String soThich;
+
+    public SinhVien() {
+    }
+
+    public SinhVien(String ten, String loai, boolean gioiTinh, String soThich) {
+        this.ten = ten;
+        this.loai = loai;
+        this.gioiTinh = gioiTinh;
+        this.soThich = soThich;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getLoai() {
+        return loai;
+    }
+
+    public void setLoai(String loai) {
+        this.loai = loai;
+    }
+
+    public boolean isGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(boolean gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getSoThich() {
+        return soThich;
+    }
+
+    public void setSoThich(String soThich) {
+        this.soThich = soThich;
+    }
+
+}
